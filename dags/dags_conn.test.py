ec1dbefa43927a7f90da74efd57bee1dca441ecd
JSLim0718 <jptofcor7@gmail.com>
2025-08-06 10:40:58 +0900

    커넥션 테스트용 flow

--- dags/dags_conn.test.py
+++ dags/dags_conn.test.py
@@ -6,7 +6,7 @@ from airflow.providers.standard.operators.empty import EmptyOperator
 with DAG(
     dag_id="dags_conn_test",
     schedule=None,
-    start_date=pendulum.datetime(2025, 8, 6, tz='Asis/Seoul'),
+    start_date=pendulum.datetime(2025, 8, 6, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     
