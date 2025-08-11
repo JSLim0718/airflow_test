a8a49649b4047c85037a01c0f293e08a63002346
JSLim0718 <jptofcor7@gmail.com>
2025-08-11 23:59:10 +0900

    python Operator 추가

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -5,7 +5,7 @@ import random
 
 with DAG(
     dag_id="dags_python_operator",
-    schedule="1 0 * * *",
+    schedule="0 0 * * *",
     start_date=pendulum.datetime(2025, 8, 11, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
