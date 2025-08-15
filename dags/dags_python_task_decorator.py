da50b721c598be41adbb696d6f1148ba415f79e0
JSLim0718 <jptofcor7@gmail.com>
2025-08-16 01:18:21 +0900

    task_decorator 업로드

---
+++ dags/dags_python_task_decorator.py
@@ -0,0 +1,16 @@
+from airflow import DAG
+from airflow.decorators import task
+import pendulum.datetime
+
+with DAG(
+    dag_id='dags_python_task_decorator',
+    schedule='30 1 * * *',
+    start_date=pendulum.datetime(2025, 8, 16, tz='Asia/Seoul'),
+    catchup=False
+    #tags=['decorator']
+) as dag:
+    @task(task_id='python_task_1')
+    def print_context(some_input):
+        print(some_input)
+        
+    python_task_1 = print_context('task_decorator 실행')
