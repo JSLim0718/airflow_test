922b7e3fec446d8df98b940311823a827adffd41
JSLim0718 <jptofcor7@gmail.com>
2025-08-17 05:22:47 +0900

    BashOperator 템플릿 dag 업로드

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -21,7 +21,8 @@ with DAG (
     
     bash_kst = PythonOperator(
         task_id = 'bash_kst',
-        python_callable = parse_date
+        python_callable = parse_date,
+        op_args = ''
     )
 
     # bash_t1 = BashOperator(
@@ -31,7 +32,7 @@ with DAG (
 
     bash_t1 = BashOperator(
         task_id = 'bash_t1',
-        bash_command= 'echo "data_interval_end: {{ task_instance.xcom_pull(key=\"kst\") }}"'
+        bash_command= 'echo "data_interval_end: {{ task_instance.xcom_pull(key=\'kst\') }}"'
     )
 
     bash_t2 = BashOperator(
