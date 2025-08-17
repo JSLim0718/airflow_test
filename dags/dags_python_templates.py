4385e7784e1593ce8782249db9b2e9acce95ab47
JSLim0718 <jptofcor7@gmail.com>
2025-08-17 22:43:06 +0900

    python_templates 업로드

---
+++ dags/dags_python_templates.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.decorators import task
+
+with DAG (
+    dag_id='dags_python_templates',
+    schedule='40 22 * * *',
+    start_date=pendulum.datetime(2025, 8, 17, tz='Asis/Seoul'),
+    catchup=False
+) as dag:
+    
+    def python_function(start_date, end_date, **kwargs):
+        print(start_date)
+        print(end_date)
+
+    python_51 = PythonOperator(
+        task_id = 'python_t1',
+        python_callable = python_function,
+        op_kwargs = {'start_date : {{ data_interval_start | ds }}', 'end_date : {{ data_interval_end | ds }}'}
+    )
+    
+    @task(task_id = 'python_t2') #task 데코레이터
+    def python_function2(**kwargs):
+        print(kwargs)
+        print('ds :' + kwargs['ds'])
+        print('ts :' + kwargs['ts'])
+        print('data_interval_start :' + str(kwargs['data_interval_start']))
+        print('data_interval_end :' + str(kwargs['data_interval_end']))
+        print('task_instance :' + str(kwargs['ti']))
