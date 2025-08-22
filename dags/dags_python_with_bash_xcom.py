78ce5d654c9076e279618d9ddfc87683e07ae603
JSLim0718 <jptofcor7@gmail.com>
2025-08-22 23:43:36 +0900

    python > bash Xcom push/pull

---
+++ dags/dags_python_with_bash_xcom.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.providers.standard.operators.python import PythonOperator
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id='dags_python_with_bash_xcom',
+    schedule='0 0 * * *',
+    start_date=pendulum.datetime(2025,8,22, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+
+    @task(task_id = 'python_push')
+    def python_push_xcom():
+        result_dict = {'status':'Good', 'data':[1,2,3], 'options_cnt':100}
+        return result_dict
+
+    bash_pull = BashOperator(
+        task_id = 'bash_pull',
+        env = {
+            'STATUS':'{{ti.xcom_pull(task_ids="python_push")["status"]}}',
+            'DATA':'{{ti.xcom_pull(task_ids="python_push")["data"]}}',
+            'OPTIONS_CNT':'{{ti.xcom_pull(task_ids="python_push")["options_cnt]}}'
+        },
+        bash_command = 'echo $STATUS && echo $DATA && echo $OPTIONS_CNT'
+    )
+
+    python_push_xcom() >> bash_pull
