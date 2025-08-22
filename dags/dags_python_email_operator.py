219d56bb568356f382b836082012f3a890d2c9e2
JSLim0718 <jptofcor7@gmail.com>
2025-08-23 01:09:34 +0900

    python >> emailoperator xcom

--- dags/dags_python_email_operator.py
+++ dags/dags_python_email_operator.py
@@ -23,7 +23,7 @@ with DAG(
         subject = '{{data_interval_end.in_timezone("Asia/Seoul") | ds}} some_logic 처리결과',
         html_content = '{{data_interval_end.in_timezone("Asia/Seoul") | ds}} 처리 결과는 <br> \
         {{ti.xcom_pull(task_ids="something_task")}} 했습니다 <br>',
-        conn_id = 'gmail_smtp'
+        conn_id = 'conn_smtp_gmail'
     )
 
     some_logic() >> send_email
