e4cc7cbbd5dbb3e634931020e142b607ca127cd8
JSLim0718 <jptofcor7@gmail.com>
2025-08-24 00:35:27 +0900

    BranchPythonOperator 분기처리

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -7,7 +7,7 @@ from airflow.providers.standard.operators.python import BranchPythonOperator
 with DAG(
     dag_id='dags_branch_python_operator',
     schedule=None,
-    start_date=pendulum.datetime(2025, 8, 24, tz='Asia.Seoul'),
+    start_date=pendulum.datetime(2025, 8, 24, tz='Asia/Seoul'),
     catchup=False
 ) as dag:
     
