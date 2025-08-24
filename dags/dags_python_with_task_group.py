fb769bbdf67e956dcc32d7a64a13d2dd84f21f04
JSLim0718 <jptofcor7@gmail.com>
2025-08-24 15:46:25 +0900

    TaskGroup 생성 및 flow 작성

--- dags/dags_python_with_task_group.py
+++ dags/dags_python_with_task_group.py
@@ -33,16 +33,16 @@ with DAG(
 
         inner_func1() >> inner_function2
     
-    with taskGroup(group_id = 'second_group', tooltip = '두 번째 그룹입니다.') as group_2:
+    with TaskGroup(group_id = 'second_group', tooltip = '두 번째 그룹입니다.') as group_2:
         '''여기에 적은 docstring은 표시되지 않습니다.''' # 이 docstring은 표시되지 않고 tooltip 파라미터로 전달한 항목이 표시
         @task(task_id = 'inner_function1')
         def inner_func1(**kwargs):
-            print('두 번째 taskGroup 내 첫 번째 task입니다.')
+            print('두 번째 TaskGroup 내 첫 번째 task입니다.')
 
         inner_function2 = PythonOperator(
             task_id = 'inner_function2',
             python_callable = inner_func,
-            op_kwargs = {'msg':'두 번째 taskGroup 내 두 번째 task입니다.'}
+            op_kwargs = {'msg':'두 번째 TaskGroup 내 두 번째 task입니다.'}
         )
         inner_func1() >> inner_function2
 
