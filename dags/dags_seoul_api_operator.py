d2fab8e583e1ee012cb905aff0b023bde56c1e20
JSLim0718 <jptofcor7@gmail.com>
2025-08-26 23:46:40 +0900

    서울시 OpenAPI 자전거 현황 데이터 ETL

---
+++ dags/dags_seoul_api_operator.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from operators.seoul_api_to_csv_operator import SeoulApiToCsvOperator
+import pendulum
+import datetime
+with DAG(
+    dag_id='dags_seoul_api_operator',
+    schedule=None,
+    start_date=pendulum.datetime(2025, 8, 26, tz='Asia/Seoul'),
+    catchup=False
+) as dag:
+    
+    '''서울시 따릉이 자전거 보관소 현황'''
+    tb_cycle_count_status = SeoulApiToCsvOperator(
+        task_id = 'tb_cycle_count_status',
+        dataset_nm = 'tbCycleStationInfo',
+        path = f'/opt/airflow/files/{dataset_nm}/' + '{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
+        file_name = 'tbCycleStationInfo.csv'
+    )
+
+    '''서울시 자전거 편의시설 현황'''
+    tb_cycle_conv_status = SeoulApiToCsvOperator(
+        task_id = 'tb_cycle_conv_status',
+        dataset_nm = 'tvBicycleEtc',
+        path = f'/opt/airflow/files/{dataset_nm}/' + '{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}',
+        file_name = 'tvBicycleEtc.csv'
+    )
+
+    tb_cycle_count_status >> tb_cycle_conv_status
