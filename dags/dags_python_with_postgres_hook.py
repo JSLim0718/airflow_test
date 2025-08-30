6c148904c73496234696028ce74f06f6a750cd7f
JSLim0718 <jptofcor7@gmail.com>
2025-08-31 03:01:01 +0900

    postgres DB 연결 with  hook

---
+++ dags/dags_python_with_postgres_hook.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.providers.standard.operators.python import PythonOperator
+
+with DAG(
+    dag_id = 'dags_python_with_postgres',
+    schedule = None,
+    start_date = pendulum.datetime(2025, 8, 31, tz='Asia/Seoul'),
+    catchup = False
+) as dag:
+
+    def insert_postgres(postgres_conn_id, **kwargs):
+        from airflow.providers.postgres.hooks.postgres import PostgresHook
+        from contextlib import closing
+
+        postgres_hook = PostgresHook(postgres_conn_id)
+        with closing(postgres_hook.get_conn()) as conn:
+            with closing(conn.cursor()) as cursor:
+                dag_id = kwargs.get('ti').dag_id
+                task_id = kwargs.get('ti').task_id
+                run_id = kwargs.get('ti').run_id
+                msg = 'insert 수행'
+                sql = 'insert into py_opr_drct_insrt values (%s,%s,%s,%s);'
+                cursor.execute(sql, (dag_id, task_id, run_id, msg))
+                conn.commit()
+
+    insert_postgres = PythonOperator(
+        task_id = 'insert_postgres',
+        python_callable = insert_postgres,
+        op_kwargs = {'postgres_conn_id':'conn-db-postgres-custom'}
+    )
+
+    insert_postgres
