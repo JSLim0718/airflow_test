49d420554cfdf6054c7afaf20c3571496b81e3ec
JSLim0718 <jptofcor7@gmail.com>
2025-08-31 02:02:03 +0900

    postgres DB insert

---
+++ dags/dags_python_with_postgres.py
@@ -0,0 +1,23 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.providers.standard.operators.python import PythonOperator
+
+def insert_postgres(ip, port, dbname, user, passwd, **kwargs):
+    import pyycopg2
+    from contextlib import closing
+    with closing(pyycopg2.connect(host=ip, dbname=dbname, user=user, password=passwd, port=int(port))) as conn:
+        with closing(conn.cursor()) as cursor:
+            dag_id = kwargs.get('ti').dag_id
+            task_id = kwargs.get('ti').task_id
+            run_id = kwargs.get('ti').run_id
+            msg = 'insert 수행'
+            sql = 'insert into py_opr_drct_insrt value (%s,%s,%s,%s);'
+            cursor.execute(sql, (dag_id, task_id, run_id, msg))
+            conn.commit()
+
+insert_postgres = PythonOperator(
+    task_id = 'insert_postgres',
+    python_callable = insert_postgres,
+    op_args = ['172.28.0.3', '5432', 'jslim', 'jslim', 'jslim'] # postgres-custom ip
+)
