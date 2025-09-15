a754ea402b8c8c57d7656f5d5176bd2570673105
JSLim0718 <jptofcor7@gmail.com>
2025-09-16 01:16:13 +0900

    dags_branch_python_operator DAG의 센서 등록

--- dags/dags_external_task_sensor.py
+++ dags/dags_external_task_sensor.py
@@ -10,28 +10,28 @@ with DAG(
     start_date = pendulum.datetime(2025, 9, 16, tz = 'Asia/Seoul'),
     catchup = False
 ) as dag:
-    external_task_sensor_A = ExternalTaskSensor(
-        task_id = 'external_task_sensor_A',
+    external_task_sensor_a = ExternalTaskSensor(
+        task_id = 'external_task_sensor_a',
         external_dag_id = 'dags_branch_python_operator', # 외부 DAG을 센싱하기 위해 로드
-        external_task_id = 'task_A',
+        external_task_id = 'task_a',
         allowed_states = [State.SKIPPED],
         execution_delta = timedelta(hours = 6),
         poke_interval = 10 # 10초
     )
 
-    external_task_sensor_B = ExternalTaskSensor(
-        task_id = 'external_task_sensor_B',
+    external_task_sensor_b = ExternalTaskSensor(
+        task_id = 'external_task_sensor_b',
         external_dag_id = 'dags_branch_python_operator',
-        external_task_id = 'task_B',
+        external_task_id = 'task_b',
         failed_states = [State.SKIPPED],
         execution_delta = timedelta(hours = 6),
         poke_interval = 10 # 10초
     )
 
-    external_task_sensor_C = ExternalTaskSensor(
-        task_id = 'external_task_sensor_C',
+    external_task_sensor_c = ExternalTaskSensor(
+        task_id = 'external_task_sensor_c',
         external_dag_id = 'dags_branch_python_operator',
-        external_task_id = 'task_C',
+        external_task_id = 'task_c',
         allowed_states = [State.SUCCESS],
         execution_delta = timedelta(hours = 6),
         poke_interval = 10 # 10초
