fd36ef6aba5d404e520b3f7fff71fe51d6838fbd
JSLim0718 <jptofcor7@gmail.com>
2025-09-23 06:20:43 +0900

    timeout 파라미터 등록

--- dags/dags_timeout_example_2.py
+++ dags/dags_timeout_example_2.py
@@ -8,26 +8,31 @@ email_str = Variable.get("email_target")
 email_1st = [email.strip() for email in email_str.split(',')]
 
 with DAG(
-    dag_id = 'dags_timeout_example_1',
+    dag_id = 'dags_timeout_example_2',
     start_date = pendulum.datetime(2025, 9, 23, tz = 'Asia/Seoul'),
     catchup = False,
     schedule = None,
     dagrun_timeout = timedelta(minutes = 1),
     default_args = {
-        'execution_timeout': timedelta(seconds = 20),
+        'execution_timeout': timedelta(seconds = 40),
         'email_on_failure' : True,
         'email' : email_1st
     }
 ) as dag:
-    bash_sleep_30 = BashOperator( # task당 timeout 기준이 20초이기 때문에 실패하는 task
-        task_id = 'bash_sleep_30',
-        bash_command = 'sleep 30'
+    bash_sleep_35 = BashOperator( # Dag의 Timeout 기준이 1분이어서 전체 Task 수행 시간이 초과하므로 DAG은 실패
+        task_id = 'bash_sleep_35',
+        bash_command = 'sleep 35'
     )
 
-    bash_sleep_10 = BashOperator(
+    bash_sleep_36 = BashOperator(
         trigger_rule = 'all_done',
-        task_id = 'bash_sleep_10',
-        bash_command = 'sleep 10'
+        task_id = 'bash_sleep_36',
+        bash_command = 'sleep 36'
     )
 
-    bash_sleep_30 >> bash_sleep_10
+    bash_go = BashOperator(
+        task_id = 'bash_go',
+        bash_command = 'exit 0'
+    )
+
+    bash_sleep_35 >> bash_sleep_36 >> bash_go
