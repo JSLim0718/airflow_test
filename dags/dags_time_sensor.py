8c645863928185a060b8d5fbacd722c840cad048
JSLim0718 <jptofcor7@gmail.com>
2025-09-24 01:49:58 +0900

    Triggerer 수행 / timesensor활용

---
+++ dags/dags_time_sensor.py
@@ -0,0 +1,15 @@
+import pendulum
+from airflow import DAG
+from airflow.providers.standard.sensors.date_time import DateTimeSensor
+
+with DAG(
+    dag_id = 'dags_time_sensor',
+    start_date = pendulum.datetime(2025, 9, 24, 0, 0, 0),
+    end_date = pendulum.datetime(2025, 9, 24, 1, 0, 0), # 0시부터 1시
+    schedule = '*/10 * * * *', # 10분마다
+    catchup = False
+) as dag:
+    sync_sensor = DateTimeSensor(
+        task_id = 'sync_sensor',
+        target_time = """{{ macros.datetime.utcnow() + macros.timedelta(minutes=5) }}"""
+    )
